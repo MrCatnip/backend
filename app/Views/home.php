7aabe169eeee3cd687121170a902e887b33422bd
MrCatnip <doe69772@gmail.com>
2026-07-01 20:50:01 +0300

    Minimalistic form for params

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,3 +1,16 @@
 <h1>Home page</h1>
 <p>Welcome to my tiny PHP MVC app 👋</p>
-<p><a href="/tasks">See the tasks →</a></p>
+
+<form method="get" action="/users" onsubmit="this.querySelectorAll('input').forEach(i => i.disabled = !i.value)">
+    <label>
+        Name
+        <input type="text" name="name" placeholder="e.g. Andrei">
+    </label>
+    <label>
+        Age
+        <input type="number" name="age" min="0" max="150" placeholder="e.g. 20">
+    </label>
+    <button type="submit">Search users</button>
+</form>
+
+<p><a href="/users">See all users →</a></p>
