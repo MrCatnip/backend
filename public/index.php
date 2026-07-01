5965e5be234ac0c18c1e3a4bde290e391e4154fe
MrCatnip <doe69772@gmail.com>
2026-07-01 20:50:39 +0300

    Oopsie

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use App\Router;
 
 $router = new Router();
 
+$router->get('/', [HomeController::class, 'get']);
+
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $router->dispatch($_SERVER['REQUEST_METHOD'], $path);
-
-$router->get('/', [HomeController::class, 'get']);
