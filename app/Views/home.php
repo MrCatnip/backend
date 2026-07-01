385df91be86c655bbaa01904ea85db9095adfd1f
MrCatnip <doe69772@gmail.com>
2026-07-01 21:08:03 +0300

    Removed dangling "?"

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,7 +1,7 @@
 <h1>Home page</h1>
 <p>Welcome to my tiny PHP MVC app 👋</p>
 
-<form method="get" action="/users" onsubmit="this.querySelectorAll('input').forEach(i => i.disabled = !i.value)">
+<form method="get" action="/users" onsubmit="event.preventDefault(); const q = new URLSearchParams([...new FormData(this)].filter(([, v]) => v.trim())).toString(); location = '/users' + (q ? '?' + q : '')">
     <label>
         Name
         <input type="text" name="name" placeholder="e.g. Andrei">
