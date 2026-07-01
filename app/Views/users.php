be2a23d32410c036c691e1c4ae66dccca79d0d3a
MrCatnip <doe69772@gmail.com>
2026-07-01 20:51:40 +0300

    Added users view

---
+++ app/Views/users.php
@@ -0,0 +1,29 @@
+<h1>Users</h1>
+<p><a href="/">← Back to home</a></p>
+
+<?php if (empty($users)): ?>
+    <p>No users found.</p>
+<?php else: ?>
+    <table>
+        <thead>
+            <tr>
+                <th>Username</th>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Age</th>
+                <th>Created</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($users as $user): ?>
+                <tr>
+                    <td><?= htmlspecialchars($user['username']) ?></td>
+                    <td><?= htmlspecialchars($user['first_name']) ?></td>
+                    <td><?= htmlspecialchars($user['last_name']) ?></td>
+                    <td><?= htmlspecialchars((string) $user['age']) ?></td>
+                    <td><?= htmlspecialchars($user['created_at']) ?></td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+<?php endif ?>
